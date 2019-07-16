6ea1bd749cd9ee7783d2fe9e11a94ad26d14ed74
Daniel Cavalcante <c0500272@vale.com>
2019-07-15 23:29:28 -0300

    Add Post Component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import Header from './src/components/Header'
+import Post from './src/components/Post'
 
 export default App = () => {
   return (
-    <View>
-      <Header></Header>
+    <View style={{flex: 1}}>
+      <Header />
+      <Post image={require('./assets/imgs/fence.jpg')} />
     </View>
   )
 }

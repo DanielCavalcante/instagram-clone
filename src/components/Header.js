9682beb514d4ca83f76a3ed4cc9406f44e507814
Daniel Cavalcante <c0500272@vale.com>
2019-07-15 23:16:58 -0300

    Add Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Platform, Image } from 'react-native'
+import Icon from '../../assets/imgs/icon.png'
+
+export default class Header extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.rowContainer}>
+          <Image source={Icon} style={styles.image}></Image>
+          <Text style={styles.title}>Instagram Clone</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === 'ios' ? 20: 0,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#BBB'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  }, 
+  image: {
+    height: 30,
+    width: 30,
+    resizeMode: 'contain'
+  },
+  title: {
+    color: '#000',
+    fontFamily: 'shelter'
+  }
+})
